1cde32e65cf52cfe101dd2dc3900864c06586171
Goran <goran@example.com>
2020-05-12 22:52:24 +0200

    setp app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,3 @@
-// console.log("Client side javascript")
-
-// // fetch('http://puzzle.mead.io/puzzle').then((res) => {
-// //     res.json().then((data) => {
-// //         console.log(data)
-// //     })
-// // })
-
-//  async function f1() {
-//     let response = await fetch('http://puzzle.mead.io/puzzle')
-//     let data = await response.json()
-//     console.log(data)
-// }
-
-// f1()
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -29,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value 
    
     messageOne.textContent = 'loading message...'
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             console.log(data.error)
